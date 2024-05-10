5311f67ad3e8a353c12a0a1ce6ba91e38077087c
tmqu130 <166485888+tmqu130@users.noreply.github.com>
2024-05-10 16:37:32 +0700

    Update bonus.sql

--- Case Study #1 - Danny's Diner/bonus.sql
+++ Case Study #1 - Danny's Diner/bonus.sql
@@ -2,9 +2,14 @@
 
 --join all the things:
 
-select S.customer_id, order_date, product_name, price, case
-    when (order_date < join_date) or (join_date is null) then 'N'
-    else 'Y'
+select 
+    S.customer_id, 
+    order_date, 
+    product_name, 
+    price, 
+    case
+        when (order_date < join_date) or (join_date is null) then 'N'
+        else 'Y'
     end as member
 into new_sales
 from sales as S 
@@ -15,11 +20,11 @@ left join members as MS
 
 --rank all the things: 
 
-select *, case
-            when member = 'N' then NULL
-            when member = 'Y' then rank() over(partition by customer_id, member order by order_date asc)
-        end as ranking
+select 
+    *, 
+    case
+        when member = 'N' then NULL
+        when member = 'Y' then rank() over(partition by customer_id, member order by order_date asc)
+    end as ranking
 into new_sales2
 from new_sales;
-
-select * from new_sales2;
