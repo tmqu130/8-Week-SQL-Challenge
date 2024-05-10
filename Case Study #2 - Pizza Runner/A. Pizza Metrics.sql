ee9abf90bed440f7006133e630e4f253b4f7db71
tmqu130 <166485888+tmqu130@users.noreply.github.com>
2024-05-10 16:59:31 +0700

    Update A. Pizza Metrics.sql

--- Case Study #2 - Pizza Runner/A. Pizza Metrics.sql
+++ Case Study #2 - Pizza Runner/A. Pizza Metrics.sql
@@ -12,7 +12,9 @@ from pizza_runner.customer_orders;
 
 --3. How many successful orders were delivered by each runner?
 
-select runner_id, count(*) as successful_order
+select 
+    runner_id, 
+    count(*) as successful_order
 from pizza_runner.runner_orders
 where cancellation = 'No'
 group by runner_id;
@@ -22,7 +24,10 @@ group by runner_id;
 alter table pizza_runner.pizza_names
 alter column pizza_name varchar(50);
 
-select PN.pizza_id, pizza_name, count(*) as order_number
+select 
+    PN.pizza_id, 
+    pizza_name, 
+    count(*) as order_number
 from pizza_runner.customer_orders as CO
 left join pizza_runner.pizza_names as PN 
     on CO.pizza_id = PN.pizza_id
@@ -33,29 +38,39 @@ group by PN.pizza_id, pizza_name;
 
 --5. How many Vegetarian and Meatlovers were ordered by each customer?
 
-select customer_id, pizza_name, count(*) as order_number
+select 
+    customer_id, 
+    pizza_name, 
+    count(*) as order_number
 from pizza_runner.customer_orders as CO
 left join pizza_runner.pizza_names as PN 
     on CO.pizza_id = PN.pizza_id
-group by customer_id, pizza_name
+group by 
+    customer_id, 
+    pizza_name
 order by customer_id;
 
 --6. What was the maximum number of pizzas delivered in a single order?
 
 with CTE AS
-    (select order_id, count(*) as pizza_number
-    from pizza_runner.customer_orders
-    group by order_id)
-select CTE.order_id, pizza_number as max_pizza_number
+    (select 
+        RO.order_id, 
+        count(*) as pizza_number
+    from pizza_runner.customer_orders as CO
+    join pizza_runner.runner_orders as RO 
+        on CO.order_id = RO.order_id
+    where cancellation = 'No'  
+    group by RO.order_id)
+select  
+    order_id, 
+    pizza_number as max_pizza_number
 from CTE
-join pizza_runner.runner_orders as RO 
-    on CTE.order_id = RO.order_id
-where cancellation = 'No' and
-    pizza_number = (select max(pizza_number) from CTE);
+where pizza_number = (select max(pizza_number) from CTE);
 
 --7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
 
-select customer_id, 
+select 
+    customer_id, 
     count(case when exclusions = '0' and extras = '0' then 1 end) as unchanged,
     count(case when exclusions <> '0' or extras <> '0' then 1 end) as changed
 from pizza_runner.customer_orders as CO  
@@ -74,16 +89,23 @@ where cancellation = 'No';
 
 --9. What was the total volume of pizzas ordered for each hour of the day?
 
-select datepart(hour, order_date) as order_hour, count(*) as pizza_order 
+select 
+    datepart(hour, order_date) as order_hour, 
+    count(*) as pizza_order 
 from pizza_runner.customer_orders
 group by datepart(hour, order_date);
 
 --10. What was the volume of orders for each day of the week?
 
-select datename (dw, order_date) as day_of_week, count(distinct order_id) as pizza_order
+select 
+    datename (dw, order_date) as day_of_week, 
+    count(distinct order_id) as pizza_order
 from pizza_runner.customer_orders
-group by datename (dw, order_date), datepart (dw, order_date)
-order by case   
-    when datepart (dw, order_date) = 1 then 7
-    else datepart (dw, order_date) - 1
+group by 
+    datename (dw, order_date), 
+    datepart (dw, order_date)
+order by 
+    case   
+        when datepart (dw, order_date) = 1 then 7
+        else datepart (dw, order_date) - 1
     end;
