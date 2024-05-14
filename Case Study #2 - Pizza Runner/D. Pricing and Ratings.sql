b6f02a68bd6ea7417c679e5737bfe2fceaf68dd6
tmqu130 <166485888+tmqu130@users.noreply.github.com>
2024-05-14 19:48:32 +0700

    Update D. Pricing and Ratings.sql

--- Case Study #2 - Pizza Runner/D. Pricing and Ratings.sql
+++ Case Study #2 - Pizza Runner/D. Pricing and Ratings.sql
@@ -21,31 +21,34 @@ from CTE;
     --Add cheese is $1 extra
 
 with CTE1 as 
-    (select row_number() over (order by order_id, pizza_id) as order_line, *
+    (select 
+        *,
+        row_number() over (order by order_id, pizza_id) as order_line
     from pizza_runner.customer_orders),
-CTE2 as  
-    (select order_line, trim(value) as extra_ingredient
+CTE2 as
+    (select 
+        order_line,
+        count(case when trim(value) <> 0 then 1 end) as ingredient_count
     from CTE1
-    cross apply string_split(extras, ',')),
-CTE3 as 
-    (select order_line, count(case when extra_ingredient <> 0 then 1 end) as ingredient_count
-    from CTE2
-    group by order_line),
-CTE4 as
-    (select CTE1.*, case
+    cross apply string_split(extras, ',')
+    group by order_line)
+select sum(Amount) as Revenue
+from
+    (select 
+        CTE1.*, 
+        case
             when pizza_name = 'Meat Lovers' then (12 + ingredient_count*1)
             when pizza_name = 'Vegetarian' then (10 + ingredient_count*1)
         end as Amount
     from CTE1 
-    join CTE3
-        on CTE1.order_line = CTE3.order_line
+    join CTE2
+        on CTE1.order_line = CTE2.order_line
     join pizza_runner.pizza_names as PN
         on CTE1.pizza_id = PN.pizza_id
     join pizza_runner.runner_orders as RO  
         on CTE1.order_id = RO.order_id
     where cancellation = 'No')
-select sum(Amount) as Revenue
-from CTE4;
+as subquery;
 
 --3. The Pizza Runner team now wants to add an additional ratings system that allows customers to rate their runner, 
     --how would you design an additional table for this new dataset
@@ -96,7 +99,8 @@ join pizza_runner.runner_orders as RO
 join pizza_runner.ratings as R 
     on CO.order_id = R.order_id
 where cancellation = 'No'
-group by customer_id, 
+group by 
+    customer_id, 
     CO.order_id, 
     runner_id, 
     rating, 
@@ -110,25 +114,19 @@ order by order_id;
 --5. If a Meat Lovers pizza was $12 and Vegetarian $10 fixed prices with no cost for extras and each runner is paid $0.30 per kilometre traveled 
     --how much money does Pizza Runner have left over after these deliveries?
 
-with CTE1A as
-    (select case 
-            when pizza_name = 'Meat Lovers' then 12
-            when pizza_name = 'Vegetarian' then 10
-        end as amount
-    from pizza_runner.customer_orders as CO
-    join pizza_runner.pizza_names as PN
-        on CO.pizza_id = PN.pizza_id
-    join pizza_runner.runner_orders as RO   
-        on CO.order_id = RO.order_id
-    where cancellation = 'No'),
-CTE1B as 
-    (select 0.3*distance as runner_cost
-    from pizza_runner.runner_orders),
-CTE2A as    
-    (select sum(amount) as revenue
-    from CTE1A),
-CTE2B as 
-    (select sum(runner_cost) as runner_cost
-    from CTE1B)
-select (revenue - runner_cost) as left_over
-from CTE2A, CTE2B;
+select
+    (
+        sum(case 
+                when pizza_name = 'Meat Lovers' then 12
+                when pizza_name = 'Vegetarian' then 10
+            end) 
+        -
+        (select sum(0.3 * distance)
+        from pizza_runner.runner_orders)
+    ) as left_over
+from pizza_runner.customer_orders as CO
+join pizza_runner.pizza_names as PN 
+    on CO.pizza_id = PN.pizza_id
+join pizza_runner.runner_orders as RO
+    on CO.order_id = RO.order_id
+where cancellation = 'No';
